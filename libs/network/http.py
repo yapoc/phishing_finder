4e8c7d53f9956fcc0d601a64396cb83574a4d99c
yapoc <yapspam@yapoc.org>
2017-11-16 09:42:24 +0100

    Type

--- libs/network/http.py
+++ libs/network/http.py
@@ -1,10 +1,15 @@
 # vi: set foldmethod=indent: set tabstop=2: set shiftwidth=2:
 import requests
+import logging
+logger = logging.getLogger (__name__)
 
 def http_get (url):
+  logger.debug ("Appel HTTP de l'URL {}".format (url))
   r = requests.get(url)
+  logger.debug ("Code retour : {}".format (r.status_code))
   if int(r.status_code) > 400:
-    msg = "Erreur d'appel GET {} ({})".format(api_endpoint, r.status_code)
+    msg = "Erreur d'appel GET {} ({})".format(url, r.status_code)
+    logger.critical (msg)
     raise Exception(msg)
   return r.text
 
